79031520193078894cce98e33400498982ca623b
anupongiv4 <anuponglive@gmail.com>
2020-07-09 23:25:56 +0700

    Updates

---
+++ precache-manifest.b2ef260f890a32d6f252507f4a22e82f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04702e15a8e012436d39da79be1842db",
+    "url": "/portfolio-am/index.html"
+  },
+  {
+    "revision": "4af0621da8ab475bded7",
+    "url": "/portfolio-am/static/css/main.cb2cad03.chunk.css"
+  },
+  {
+    "revision": "2e4ccef2888b37c46d7f",
+    "url": "/portfolio-am/static/js/2.07352adf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-am/static/js/2.07352adf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4af0621da8ab475bded7",
+    "url": "/portfolio-am/static/js/main.60c64253.chunk.js"
+  },
+  {
+    "revision": "c33b5a4f9d6de97c52e4",
+    "url": "/portfolio-am/static/js/runtime-main.3981f484.js"
+  },
+  {
+    "revision": "0f1df42e69da01bb61496a90fb517416",
+    "url": "/portfolio-am/static/media/background-banner.0f1df42e.jpg"
+  },
+  {
+    "revision": "fecf63f00845ae2ad153e5b507a75924",
+    "url": "/portfolio-am/static/media/creditcard-checker.fecf63f0.png"
+  },
+  {
+    "revision": "7428c0e4a2d2b5c1314d3f98f7ccbcbb",
+    "url": "/portfolio-am/static/media/facebook-logo.7428c0e4.png"
+  },
+  {
+    "revision": "9260c7748ae909153ba2f9bc78fe0b88",
+    "url": "/portfolio-am/static/media/github-mark.9260c774.png"
+  },
+  {
+    "revision": "7b516dfd1494af9e40c083330d273ade",
+    "url": "/portfolio-am/static/media/mini-weather-app.7b516dfd.png"
+  },
+  {
+    "revision": "abcf95a635c2d4dff158766b490985e7",
+    "url": "/portfolio-am/static/media/monitor.abcf95a6.png"
+  },
+  {
+    "revision": "31b0f2b37f2f7dd70c45931fd29c2dca",
+    "url": "/portfolio-am/static/media/paint-brush.31b0f2b3.png"
+  },
+  {
+    "revision": "2c6e5e1e1c216a2704384f5f58f3072f",
+    "url": "/portfolio-am/static/media/project-1.2c6e5e1e.jpg"
+  },
+  {
+    "revision": "c937d3a8918a8bbe63ccf6630bd3deba",
+    "url": "/portfolio-am/static/media/project-3.c937d3a8.jpg"
+  },
+  {
+    "revision": "e45b22a96c8cb7dcc706b8bbfecda456",
+    "url": "/portfolio-am/static/media/twitter-logo.e45b22a9.png"
+  }
+]);
